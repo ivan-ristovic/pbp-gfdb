14c28c0886dba4be7705763516b912fd0be2614f
ivan-ristovic <ivo.ristovic@gmail.com>
2017-12-12 00:33:11 +0100

    [App] Nove funkcije

--- app/src/command_handler.c
+++ app/src/command_handler.c
@@ -6,22 +6,64 @@
 
 void process_command(char **parsed_data, int arg_c)
 {
-    if (!strcmp(parsed_data[0], "exit"))
+    if (!strcmp(parsed_data[0], "exit")) {
         exit(EXIT_SUCCESS);
-    else if (!strcmp(parsed_data[0], "delguild"))
-        printf("delguild");
-    else if (!strcmp(parsed_data[0], "addmember"))
-        printf("addmember");
-    else if (!strcmp(parsed_data[0], "updmember"))
-        printf("updmember");
-    else if (!strcmp(parsed_data[0], "ban"))
-        printf("ban");
-    else if (!strcmp(parsed_data[0], "unban"))
-        printf("unban");
-    else if (!strcmp(parsed_data[0], "nickname"))
-        printf("nickname");
-    else if (!strcmp(parsed_data[0], "q"))
-        printf("q");
-    else
+    } else if (!strcmp(parsed_data[0], "delguild")) {
+        delguild(parsed_data);
+    } else if (!strcmp(parsed_data[0], "addmember")) {
+        addmember(parsed_data);
+    } else if (!strcmp(parsed_data[0], "updmember")) {
+        updmember(parsed_data);
+    } else if (!strcmp(parsed_data[0], "ban")) {
+        ban(parsed_data);
+    } else if (!strcmp(parsed_data[0], "unban")) {
+        unban(parsed_data);
+    } else if (!strcmp(parsed_data[0], "nickname")) {
+        nickname(parsed_data);
+    } else if (!strcmp(parsed_data[0], "q")) {
+        q(parsed_data);
+    } else {
         printf("Unknown command.");
+    }
+}
+
+
+void delguild(char **parsed_data)
+{
+    printf("delguild");
+}
+
+void addmember(char **parsed_data)
+{
+    printf("addmember");
+}
+
+
+void updmember(char **parsed_data)
+{
+    printf("updmember");
+}
+
+
+void ban(char **parsed_data)
+{
+    printf("ban");
+}
+
+
+void unban(char **parsed_data)
+{
+    printf("unban");
+}
+
+
+void nickname(char **parsed_data)
+{
+    printf("nickname");
+}
+
+
+void q(char **parsed_data)
+{
+    printf("q");
 }
--- app/src/command_handler.h
+++ app/src/command_handler.h
@@ -2,5 +2,12 @@
 #define _H_COMMAND_HANDLER_H_
 
 void process_command(char **parsed_data, int arg_c);
+void delguild(char **parsed_data);
+void addmember(char **parsed_data);
+void updmember(char **parsed_data);
+void ban(char **parsed_data);
+void unban(char **parsed_data);
+void nickname(char **parsed_data);
+void q(char **parsed_data);
 
 #endif
